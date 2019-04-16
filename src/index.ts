e1aa6f8fd1f21716a2a7c99697f2433f6447d46b
CastleMY <490523604@qq.com>
2019-04-16 14:22:54 +0800

    update d.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
-import RPCClient from '@ctsy/rpc-client/dist/index';
+import RPCClient from '@ctsy/rpc-client';
 declare let require: any;
 const ws = require('isomorphic-ws')
 export default class RPC extends RPCClient {
     constructor(URL: string, Name: string) {
         super(URL, Name, ws)
-    }
-    registServiceByObject(pathSuffix: string, obj: any) {
-
     }
 }
